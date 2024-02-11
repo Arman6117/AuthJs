d29a012449b6d43af3b3f56b5304daa4af2905fb
Arman Patel <vanixgaming634@gmail.com>
2024-02-11 15:59:20 +0530

    style:Height fix

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex  flex-col h-screen  justify-center items-center bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-indigo-600 to-purple-600">
+  return <div className="flex  flex-col h-full  justify-center items-center bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-indigo-600 to-purple-600">
     {children}
   </div>;
 };
